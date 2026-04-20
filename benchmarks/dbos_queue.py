11661bc9e24200a86875568404ce1625a1895c4d
Peter Kraft <petereliaskraft@gmail.com>
2026-04-20 11:01:54 -0700

    nit

--- benchmarks/dbos_queue.py
+++ benchmarks/dbos_queue.py
@@ -82,7 +82,7 @@ def worker_entry(
     # Create all queues in every worker so any worker can enqueue to any queue.
     # Workflows are enqueued to a random queue per call.
     queues = [
-        Queue(f"bench-queue-{i}", worker_concurrency=1000)
+        Queue(f"bench-queue-{i}", worker_concurrency=1000, polling_interval_sec=0.1)
         for i in range(num_queues)
     ]
 
